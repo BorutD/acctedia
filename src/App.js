65bb0bd91a3509caefcaba39eb25c6adcf3972a3
BorutD <borut.drazumeric@gmail.com>
2020-02-10 10:51:55 +0100

    Added navbar and three pages.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Acctedia</h1>
-      </header>
-    </div>
-  );
+// Pages
+import home from "./pages/home";
+import login from "./pages/login";
+import signup from "./pages/signup";
+
+// Components
+import Navbar from "./components/layout/Navbar";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Router>
+          <Navbar />
+          <div className="container">
+            <Switch>
+              <Route exact path="/" component={home} />
+              <Route exact path="/login" component={login} />
+              <Route exact path="/signup" component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
 }
 
 export default App;
